7f1e69b9b82600c22b1f5ac196ec808d5e96a62f
WinterCore <hogobbl@gmail.com>
2020-07-14 21:22:55 +0200

    Fixed rubocop and stylelint errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,12 +28,12 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
     flash.notice = "Article '#{@article.title}' Updated!"
-  
+
     redirect_to article_path(@article)
   end
 end
