84614e31579152fa53c627df0d1a4e0f72169aed
Bismarck-GM <geromorla@yahoo.com.ar>
2020-07-15 19:28:47 -0300

    Fix some rubocop offenses and added more

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,10 +9,10 @@ class AuthorsController < ApplicationController
       return false
     end
   end
+
   # GET /authors
   # GET /authors.json
   def index
-
     @authors = Author.all
   end
 
@@ -71,13 +71,14 @@ class AuthorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def author_params
-      params.require(:author).permit(:username, :email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def author_params
+    params.require(:author).permit(:username, :email, :password, :password_confirmation)
+  end
 end
